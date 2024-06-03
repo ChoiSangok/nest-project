a503990181a9f4a27567e7ede0c42c3e1070d368
ChoiSangOk <tkddhr12345@naver.com>
2024-06-03 16:45:33 +0900

    Feat: typeorm

---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'choi6882@',
+  database: 'Board-app', // database 이름
+  entities: [__dirname + '/../**/*.entity.{js,ts}'], // 사용할 entity의 클래스명
+  synchronize: true,
+};
